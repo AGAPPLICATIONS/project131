6256dabdcc5c281130bd5c421ce444d77ac83b9c
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2023-09-02 12:58:02 +0530

    Add files via upload

---
+++ human.js
@@ -0,0 +1,27 @@
+img="";
+status1="";
+function preload(){
+    img= loadImage("1.jpg");
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectdetector=ml5.objectDetector('cocossd',modelloaded);
+    document.getElementById("status").innerHTML="Status:Detecting Objects";
+
+}
+function modelloaded(){
+    console.log("Model is Loaded");
+    status1=true;
+    objectdetector.detect(img,gotresults);
+}
+function gotresults(error,results){
+if(error){
+    console.log(error);
+}else{
+    console.log(results);
+}
+}
+function draw(){
+ image(img,0,0,640,420);
+}
