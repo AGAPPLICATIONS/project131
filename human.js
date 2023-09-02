f978460b7e4eebe31b45afafde8d5d2e7ef6dd0e
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2023-09-02 16:44:46 +0530

    Add files via upload

--- human.js
+++ human.js
@@ -1,5 +1,6 @@
 img="";
 status1="";
+objects=[];
 function preload(){
     img= loadImage("1.jpg");
 }
@@ -20,8 +21,21 @@ if(error){
     console.log(error);
 }else{
     console.log(results);
+    objects=results;
 }
 }
 function draw(){
- image(img,0,0,640,420);
-}
+    image(img,0,0,640,420);
+     if (status1!="") {
+       for (i = 0; i < objects.length; i++) {
+           document.getElementById("status").innerHTML = "Status: Objects Detected";
+           fill("red");
+           percent=floor(objects[i].confidence*100);
+           text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+           noFill();
+           stroke("red");
+           rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+           
+       }
+     }
+   }
